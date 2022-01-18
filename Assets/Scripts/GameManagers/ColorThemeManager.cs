192bece6b625f37372bcfa435c2249984cace007
davidchermanto <davidchermanto@gmail.com>
2022-01-19 01:03:21 +1100

    Test Commit

--- Assets/Scripts/GameManagers/ColorThemeManager.cs
+++ Assets/Scripts/GameManagers/ColorThemeManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ColorThemeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Setup()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
